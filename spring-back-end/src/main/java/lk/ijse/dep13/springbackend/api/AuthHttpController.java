405b806022b6b24194c1b9f321b8f325817bcd95
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-04-03 21:35:03 +0530

    Completed the Authentication and the improving the code readability by removing the unnessory things

--- spring-back-end/src/main/java/lk/ijse/dep13/springbackend/api/AuthHttpController.java
+++ spring-back-end/src/main/java/lk/ijse/dep13/springbackend/api/AuthHttpController.java
@@ -1,8 +1,8 @@
 package lk.ijse.dep13.springbackend.api;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lk.ijse.dep13.springbackend.entity.User;
-import org.apache.commons.codec.cli.Digest;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,28 +27,29 @@ public class AuthHttpController {
         try (var stm = connection.prepareStatement("SELECT * FROM \"user\" WHERE email=?")) {
             stm.setString(1, user.getEmail());
             ResultSet rst = stm.executeQuery();
-            if (rst.next()) {
-                String actualPassword = rst.getString("password");
-                String encryptedPassword = DigestUtils.sha256Hex(user.getPassword());
-                if (actualPassword.equals(encryptedPassword)) {
-                    request.getSession().setAttribute("user", user.getEmail());
-                    String fullName = rst.getString("full_name");
-                    // requireNonNullElse() is using for test and set the image
-                    String profilePicture = Objects.requireNonNullElse(rst
-                            .getString("profile_picture"), User.DEFAULT_PROFILE_PICTURE);
-                    return new User(fullName, user.getEmail(), encryptedPassword, profilePicture);
-                }else{
-                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
-                }
-            }else{
+            // Readability Improvement
+            if (rst.next()) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
+
+            String actualPassword = rst.getString("password");
+            String encryptedPassword = DigestUtils.sha256Hex(user.getPassword());
+            // Readability Improvement
+            if (!actualPassword.equals(encryptedPassword))
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
-            }
+
+            request.getSession().setAttribute("user", user.getEmail());
+            String fullName = rst.getString("full_name");
+            // requireNonNullElse() is using for test and set the image
+            String profilePicture = Objects.requireNonNullElse(rst
+                    .getString("profile_picture"), User.DEFAULT_PROFILE_PICTURE);
+            return new User(fullName, user.getEmail(), encryptedPassword, profilePicture);
+
         }
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/logout")
-    public String logOut() {
-        return "Log out";
+    public void logOut(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) session.invalidate();
     }
 }
