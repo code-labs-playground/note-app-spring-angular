1ec0a6d4d5369572bd3b9737eeb6d158ba3a1194
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-04-03 20:38:52 +0530

    Making the Image base64 url

---
+++ spring-back-end/src/test/java/ProfilePictureGeneratorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Base64;
+
+public class ProfilePictureGeneratorTest {
+
+    @Test
+    void generateBase64DataUrl() throws IOException {
+        byte[] bytes = getClass()
+                .getResourceAsStream("/profile-picture.png")
+                .readAllBytes();
+        String base64Url = Base64.getEncoder().encodeToString(bytes);
+        System.out.println("data:image/png;base64," + base64Url);
+    }
+}
